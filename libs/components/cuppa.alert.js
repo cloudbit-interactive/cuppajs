7dbdb41a120869d7f168132cbfac46c861b5923d
Tufik Chediak <tufik2@gmail.com>
2021-02-16 23:36:10 -0500

    Update cuppa.alert.js
    Small implementation

--- libs/components/cuppa.alert.js
+++ libs/components/cuppa.alert.js
@@ -37,8 +37,8 @@ export default class CuppaAlert extends CuppaComponent {
         return /*html*/`
             <style>
                 html, body{ overflow:hidden; }
-                cuppa-alert{ animation-name: cuppa_alert_animation; animation-duration: 0.2s; position:fixed; left:0; right:0; top:0; bottom:0; display:flex; justify-content:center; align-items:center; padding:20px; }
-                .cuppa_alert_blockade{ background:rgba(0,0,0,0.5); position:absolute; left:0; right:0; top:0; bottom:0; }
+                cuppa-alert{ animation-name: cuppa_alert_animation; animation-duration: 0.2s; position:fixed; left:0; right:0; top:0; bottom:0; display:flex; justify-content:center; align-items:center; padding:20px; overflow:auto; }
+                .cuppa_alert_blockade{ background:rgba(0,0,0,0.5); position:fixed; left:0; right:0; top:0; bottom:0; }
                 .cuppa_alert_modal{ position:relative; background: #FFF; width:100%; max-width:500px; padding:30px 40px; overflow: hidden; border-radius: 5px; box-shadow: 0px 3px 10px rgba(0,0,0,0.3); }
                 .cuppa_alert_title{ font-size: 22px; font-weight: 700; margin:0 0 10px; }
                 .cuppa_alert_input{ width:100%; height:32px; width: 100%; margin:10px 0 0; background: #FFF; color: #333; border-radius: 3px; border: 1px solid #CCC; box-shadow:inset 0 1px 1px rgba(0,0,0,.075); padding:0 10px;  font-weight: 500;  }
