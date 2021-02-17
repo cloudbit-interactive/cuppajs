8300459d8f069302187c4d52a30e3decbb235573
Tufik Chediak <tufik2@gmail.com>
2021-02-16 23:12:33 -0500

    Create Alert Component

--- examples/src/components/sections/cuppaComponents/CuppaComponents.js
+++ examples/src/components/sections/cuppaComponents/CuppaComponents.js
@@ -1,6 +1,7 @@
+import CuppaSwitch from "../../../../../libs/components/cuppa.switch.js";
 import {CuppaComponent} from "../../../../../libs/cuppa.component.js";
-
 import("../../../../../libs/components/cuppa.switch.js");
+import("../../../../../libs/components/cuppa.alert.js");
 
 export default class CuppaComponents extends CuppaComponent {
     
@@ -12,14 +13,24 @@ export default class CuppaComponents extends CuppaComponent {
         console.log(e.currentTarget.state)
     }
 
+    onAlert(){
+        document.body.append(new CuppaAlert({title:"Hi,", message:"Please specify your name", inputText:"", cancelText:"Cancel", callback:(value, input)=>console.log(value, input)} ));
+    }
+
     render(){
         return /*html*/`
             <div>
                 <h2 class="title2">How use a component created in any framework</h2>
                 <p>There are tons of way to load a components depending of each case. Bellow is showed the simples and fast way to import it.</p>
                 
+                <h3 class="title3">CuppaSwitch</h3>
                 <cuppa-switch name="switch1" onChange="this.onChange" ></cuppa-switch>
+                <!--
                 <iframe style="height:600px;" src="https://stackblitz.com/edit/cuppa-component-import1?embed=1&file=index.html&hideExplorer=1&hideNavigation=1"></iframe>
+                -->
+
+                <h3 class="title3">Cuppa Alert</h3>
+                <button onclick="this.onAlert">Show Alert</button>
             </div>`
     }
 }
---
+++ libs/components/cuppa.alert.js
@@ -0,0 +1,69 @@
+import {CuppaComponent} from "../../../../libs/cuppa.component.js";
+import {cuppa} from "../../../../libs/cuppa.js";
+
+export default class CuppaAlert extends CuppaComponent {
+    state = {title:"", message:"", htmlContent:"", acceptText:"Accept", cancelText:"", backdropEnabled:true, inputText:null, placeholder:"", callback:null}
+
+    constructor(opts = {title:"", message:"", htmlContent:null, acceptText:"Accept", cancelText:"", backdropEnabled:true, inputText:null, placeholder:"", callback:null}){
+        super();
+        this.state = {...this.state, ...opts};
+    }
+
+    static get observedAttributes() { return ['title', 'message', 'accept-text', 'cancel-text', 'backdrop-enabled', 'input-text', 'placeholder'] }
+    attributeChangedCallback(attr, oldVal, newVal) {
+        this.setState({[cuppa.camelize(attr)]:newVal});
+     }
+
+    connected(){
+        if(this.state.htmlContent){
+            this.querySelector(".cuppa_alert_html_content").append(this.state.htmlContent);
+        }
+    }
+
+    onClick(value){
+        if(this.state.callback) this.state.callback(value, this.state.inputText, this);
+        this.close();
+    }
+
+    onChangeInput(e){
+        this.setState({inputText:e.currentTarget.value})
+    }
+
+    close(){
+        this.parentNode.removeChild(this);
+    }
+
+    render(){
+        return /*html*/`
+            <style>
+                html, body{ overflow:hidden; }
+                cuppa-alert{ position:fixed; left:0; right:0; top:0; bottom:0; display:flex; justify-content:center; align-items:center; padding:20px; }
+                .cuppa_alert_blockade{ background:rgba(0,0,0,0.5); position:absolute; left:0; right:0; top:0; bottom:0; }
+                .cuppa_alert_modal{ position:relative; background: #FFF; width:100%; max-width:400px; padding:30px 40px; overflow: hidden; border-radius: 5px; box-shadow: 0px 3px 10px rgba(0,0,0,0.3); }
+                .cuppa_alert_title{ font-size: 22px; font-weight: 700; margin:0 0 10px; }
+                .cuppa_alert_input{ width:100%; height:32px; width: 100%; margin:10px 0 0; background: #FFF; color: #333; border-radius: 3px; border: 1px solid #CCC; box-shadow:inset 0 1px 1px rgba(0,0,0,.075); padding:0 10px;  font-weight: 500;  }
+                .cuppa_alert_buttons{ margin:10px 0 0; display:flex; justify-content:flex-end; }
+                .cuppa_alert_button{ transition: 0.3s background-color; border: none; cursor:pointer; background: #2F80EC; color:#FFF; height: 42px; padding:0 15px; margin:0 3px; border-radius: 3px; }
+                .cuppa_alert_button:hover{ background: #1a62c1; }
+                .cuppa_alert_button_cancel{ background:#CCC; color:#333;  }
+                .cuppa_alert_button_cancel:hover{ background:#A0A0A0; }
+            </style>
+            <div ${!this.state.backdropEnabled ? '' : `onclick="this.onClick(false)"`} class="cuppa_alert_blockade"></div>
+            <div class="cuppa_alert_modal">
+                ${this.state.title ? /*html*/`<div class="cuppa_alert_title">${this.state.title}</div>` : ''}
+                <div class="cuppa_alert_message">${this.state.message}</div>
+                ${this.state.htmlContent ? /*html*/`<div class="cuppa_alert_html_content"></div>` : '' }
+                ${this.state.inputText == undefined ? '' : /*html*/`<input class="cuppa_alert_input" value="${this.state.inputText}" oninput="this.onChangeInput" placeholder="${this.state.placeholder}" />`}
+                ${ this.state.canceText || this.state.acceptText ? /*html*/`
+                    <div class="cuppa_alert_buttons">
+                        ${ !this.state.cancelText ? '' : /*html*/`<button onclick="this.onClick(false)" class="cuppa_alert_button cuppa_alert_button_cancel">${this.state.cancelText}</button>`}
+                        ${ !this.state.acceptText ? '' : /*html*/`<button onclick="this.onClick(true)" class="cuppa_alert_button cuppa_alert_button_accept">${this.state.acceptText}</button>`}
+                    </div>
+                ` : '' }
+            </div>
+            `
+    }
+}
+
+customElements.define('cuppa-alert', CuppaAlert);
+document.defaultView.CuppaAlert = CuppaAlert;
