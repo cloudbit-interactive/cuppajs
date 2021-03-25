c61bf053d11d06040145d17ea95d14dd9c61d157
Tufik Chediak <tufik2@gmail.com>
2021-03-24 22:24:45 -0400

    Documentation update

--- docs/src/components/sections/basic/cuppaRouter.js
+++ docs/src/components/sections/basic/cuppaRouter.js
@@ -7,14 +7,14 @@ export default class CuppaRouter extends CuppaComponent {
             <div class="grid_title_1_column">
                 <h2 class="title2" style="grid-area:title">Counter Router</h2>
                 <div style="grid-area:content">
-                    <div class="message message_gray" style="border-radius:5px 5px 0 0">Create simple router in pure vanilla javascript or integrate it in any framework.</div>
+                    <div class="message" style="border-radius:5px 5px 0 0">Create simple router in pure vanilla javascript or integrate it in any framework.</div>
                     <iframe height="550" style="width: 100%;" src="https://codesandbox.io/embed/distracted-matsumoto-0k2kf?fontsize=14&theme=dark" ></iframe>
                 </div>
                 
             </div>
             <hr class="separator1"/>
             <div class="grid_title_1_column">
-                <h2 class="title2" style="grid-area:title;">Using Resolve Function</h2>
+                <h2 class="title2" style="grid-area:title;">Using Resolve Method</h2>
                 
             </div>
         `
