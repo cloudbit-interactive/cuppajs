8f5a8923e368fb980ed0e181746517159705a687
Tufik Chediak <tufik2@gmail.com>
2021-03-24 23:28:58 -0400

    Update documentation

--- docs/src/components/sections/basic/cuppaRouter.js
+++ docs/src/components/sections/basic/cuppaRouter.js
@@ -5,17 +5,20 @@ export default class CuppaRouter extends CuppaComponent {
     render(){
         return /*html*/`
             <div class="grid_title_1_column">
-                <h2 class="title2" style="grid-area:title">Counter Router</h2>
+                <h2 class="title2" style="grid-area:title">Cuppa Router</h2>
                 <div style="grid-area:content">
                     <div class="message" style="border-radius:5px 5px 0 0">Create simple router in pure vanilla javascript or integrate it in any framework.</div>
                     <iframe height="550" style="width: 100%;" src="https://codesandbox.io/embed/distracted-matsumoto-0k2kf?fontsize=14&theme=dark" ></iframe>
                 </div>
-                
             </div>
             <hr class="separator1"/>
             <div class="grid_title_1_column">
-                <h2 class="title2" style="grid-area:title;">Using Resolve Method</h2>
-                
+                <h2 class="title2" style="grid-area:title;">Using On Method</h2>
+                <iframe height="550" style="width: 100%;" src="https://codesandbox.io/embed/cupparouter-resolver-7v1us?fontsize=14&theme=dark" ></iframe>
+            </div>
+            <hr class="separator1"/>
+            <div class="grid_title_1_column">
+                <h2 class="title2" style="grid-area:title;">Adding Predefined Title Map</h2>
             </div>
         `
     }
