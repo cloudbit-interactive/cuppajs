5f09a053048afac7910add4eb1992bfdb243b8ed
Tufik Chediak <tufik2@gmail.com>
2021-04-09 17:10:32 -0400

    Update cuppaStorage.js

--- docs/src/components/sections/basic/cuppaStorage.js
+++ docs/src/components/sections/basic/cuppaStorage.js
@@ -16,7 +16,6 @@ export default class CuppaStorageBase extends CuppaComponent {
 
     render(){
         return /*html*/`
-            <!--
             <div class="grid_title_1_column">
                 <h2 class="title2" style="grid-area:title;">Basic Usage</h2>
                 <iframe style="grid-are:content" height="450" style="width: 100%;" src="https://codepen.io/tufik2/embed/poRWKPE?&theme-id=dark&default-tab=js,result" frameborder="no" loading="lazy" allowtransparency="true" allowfullscreen="true"></iframe>
@@ -26,7 +25,6 @@ export default class CuppaStorageBase extends CuppaComponent {
                 <h2 class="title2" style="grid-area:title;">Storage And React</h2>
                 <iframe style="grid-are:content" height="450" style="width: 100%;" src="https://codepen.io/tufik2/embed/abpLjOQ?&theme-id=dark&default-tab=js,result" frameborder="no" loading="lazy" allowtransparency="true" allowfullscreen="true"></iframe>
             </div>
-            -->
         `
     }
 }
