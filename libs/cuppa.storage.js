c65d1bff001c5a56ae0a208013996d9f7aeabe36
Tufik Chediak <tufik2@gmail.com>
2021-09-23 22:41:18 -0400

    Update CuppaStorage

--- libs/cuppa.storage.js
+++ libs/cuppa.storage.js
@@ -2,38 +2,83 @@
  *  CuppaStorage
  * 
  * **/
-
 export class CuppaStorage{
-    static async setData(name, opts){
-        return await cuppa.setData(name, opts);
+    static LOCAL = "LOCAL";
+    static SESSION = "SESSION";
+    static INDEXED_DB = "INDEXED_DB";
+    static db;
+    static defaultData = {};
+    static data = {};
+    static callbacks = {};
+
+    static async setData({name = 'default', data = null, store = '', silence = false}){
+        if(String(store).toUpperCase() === CuppaStorage.LOCAL){
+            localStorage.setItem(name, JSON.stringify(data));
+        }else if(String(store).toUpperCase() === CuppaStorage.SESSION){
+            sessionStorage.setItem(name, JSON.stringify(data));
+        }else if(String(store).toUpperCase() === CuppaStorage.INDEXED_DB){
+            await CuppaStorage.db.add(name, data);
+        }else{
+            CuppaStorage.data[name] = data;
+        }
+
+        if(!silence) CuppaStorage.executeCallbacks({name, data});
     }
 
-    static async getData(name, opts){
-        return await cuppa.getData(name, opts);
+    static async getData({name = 'default', callback = null, store = '', defaultValue = null,}){
+        let data;
+        if(String(store).toUpperCase() === CuppaStorage.LOCAL){
+            let ls = localStorage.getItem(name);
+            if(ls) data = JSON.parse(ls);
+        }else if(String(store).toUpperCase() === CuppaStorage.SESSION){
+            let st = sessionStorage.getItem(name);
+            if(st) data = JSON.parse(st);
+        }else if(String(store).toUpperCase() === CuppaStorage.INDEXED_DB){
+            data = await CuppaStorage.db.get(name);
+        }else{
+            data = CuppaStorage.data[name];
+        }
+
+        if(data === undefined){ data = defaultValue; }
+
+        if(data != undefined && callback){ callback(data); };
+        if(callback){ CuppaStorage.addCallback({name, callback}); };
+        return data;
     }
 
-    static async deleteData(name, opts){
-        await cuppa.deleteData(name, opts);
+    static removeCallback({name, callback, toString = false}){
+        if(!CuppaStorage.callbacks[name]) return;
+        let array = CuppaStorage.callbacks[name];
+        for(let i = 0 ; i < array.length; i++ ){
+            if(toString){
+                if(array[i].toString() === callback.toString()){ array.splice(i, 1); };
+            }else{
+                if(array[i] === callback){ array.splice(i, 1); };
+            }
+        };
     };
 
-    static removeCallback(name, callback){
-        cuppa.removeListener(name, callback);
-    }
+    static removeAllCallbacks({name}){
+        delete CuppaStorage.callbacks[name];
+    };
 
-    static removeListener(name, callback){
-        CuppaStorage.removeCallback(name, callback);
-    }
+    static executeCallbacks({name, data}){
+        if(!CuppaStorage.callbacks[name]) return;
+        let array = CuppaStorage.callbacks[name];
+        for(let i = 0 ; i < array.length; i++){
+            array[i](data);
+        };
+    };
 
-    static removeAllCallbacks(name){
-        cuppa.removeListenerGroup(name);
-    }
-}
+    static addCallback = function({name, callback}){
+        if(!CuppaStorage.callbacks[name]) CuppaStorage.callbacks[name] = [];
+        CuppaStorage.callbacks[name].push(callback);
+    };
+};
 
-CuppaStorage.LOCAL = "LOCAL";
-CuppaStorage.SESSION = "SESSION";
-CuppaStorage.INDEXED_DB = "INDEXED_DB";
-
-document.defaultView.CuppaStorage = CuppaStorage;
+if(!document.defaultView.CuppaStorage){
+    document.defaultView.CuppaStorage = CuppaStorage;
+};
 
 export class GetStorage extends HTMLElement{
     name;
@@ -44,136 +89,30 @@ export class GetStorage extends HTMLElement{
     constructor(){
         super();
         this.onUpdateStorage = this.onUpdateStorage.bind(this);
-    }
+    };
 
     connectedCallback() {
         setTimeout(()=>{
             this.name = this.getAttribute("name");
             this.store = this.getAttribute("store");
             this.default = this.getAttribute("default");
-            CuppaStorage.getData(this.name, {callback:this.onUpdateStorage, default:this.default, store:this.store});
+            CuppaStorage.getData({name:this.name, callback:this.onUpdateStorage.bind(this), default:this.default, store:this.store}).then();
         }, 0);
-    }
+    };
 
     onUpdateStorage(data){
         this.data = data;
         this.dispatchEvent(new CustomEvent("update", {detail:this.data}));
-    }
+    };
 
     disconnectedCallback(){
-        CuppaStorage.removeCallback(this.name, this.onUpdateStorage);
-    }
-}
-
-customElements.define('get-storage', GetStorage);
-document.defaultView.GetStorage = GetStorage;
-
-/*  DataCenter
-    add and remove data in one place
-    Examples:
-    cuppa.setData('NEW_CLIENT', {data:{name:"Tufik", age":18}})
-    cuppa.getData('NEW_CLIENT', {callback:listener});
-*/
-const cuppa = document.defaultView.cuppa || {};
-cuppa.dataDefault = cuppa.dataDefault || {};
-cuppa.data = cuppa.data || {};
-
-cuppa.setData = async function(name, opts){
-    opts = {...{storage:'', silence:false, data:null}, ...opts};
-    if(opts.store != undefined) opts.storage = opts.store;
-
-    if(opts["default"] !== undefined && opts["default"] !== null){
-        cuppa.dataDefault[name] = opts["default"];
-        opts["default"] = null;
-        let current = cuppa.getData(name, opts);
-        if(current == null || current == undefined) current = cuppa.dataDefault[name];
-        opts.data = current;
-        cuppa.setData(name, opts).then();
-        return;
-    }
-
-    cuppa.data[name] = opts.data;
-    if(String(opts.storage).toUpperCase() === CuppaStorage.LOCAL){
-        localStorage.setItem(name, JSON.stringify(opts.data));
-    }else if(String(opts.storage).toUpperCase() === CuppaStorage.SESSION){
-        sessionStorage.setItem(name, JSON.stringify(opts.data));
-    }else if(String(opts.storage).toUpperCase() === CuppaStorage.INDEXED_DB){
-        await CuppaStorage.db.add(name, opts.data);
-    }
-
-    if(!opts.silence) cuppa.executeListener(name, opts.data);
-};
-
-cuppa.getData = async function(name, opts){
-    opts = {...{storage:'', callback:null, "default":null}, ...opts};
-    if(opts.store != undefined) opts.storage = opts.store;
-
-    let data = cuppa.data[name];
-    if(String(opts.storage).toUpperCase() === CuppaStorage.LOCAL){
-        let ls = localStorage.getItem(name);
-        if(ls) data = JSON.parse(ls);
-    }else if(String(opts.storage).toUpperCase() === CuppaStorage.SESSION){
-        let st = sessionStorage.getItem(name);
-        if(st) data = JSON.parse(st);
-    }else if(String(opts.storage).toUpperCase() === CuppaStorage.INDEXED_DB){
-       data = await CuppaStorage.db.get(name);
-    }
-
-    if(data === undefined){ data = opts["default"]; }
-
-    if(data != undefined && opts.callback){ opts.callback(data); };
-    if(opts.callback){ cuppa.addListener(name, opts.callback); };
-    return data;
-};
-
-cuppa.deleteData = async function(name, opts){
-    opts = {...{storage:''}, ...opts};
-    if(opts.store != undefined) opts.storage = opts.store;
-    cuppa.data[name] = null;
-
-    if(opts.storage === "local"){
-        localStorage.removeItem(name);
-    }else if(opts.storage === "session"){
-        sessionStorage.removeItem(name);
-    }else if(String(opts.storage).toUpperCase() === CuppaStorage.INDEXED_DB){
-        await CuppaStorage.db.delete(name);
-    }
-};
-
-// add / remove / execute global listeners
-cuppa.listeners = cuppa.listeners || {};
-
-cuppa.addListener = function(name, callback){
-    if(!cuppa.listeners[name]) cuppa.listeners[name] = [];
-    cuppa.listeners[name].push(callback);
-};
-
-cuppa.removeListener = function(name, callback, toString){
-    if(!cuppa.listeners[name]) cuppa.listeners[name] = [];
-    let array = cuppa.listeners[name];
-    for(let i = 0 ; i < array.length; i++ ){
-        if(toString){
-            if(array[i].toString() === callback.toString()){
-                array.splice(i, 1);
-            };
-        }else{
-            if(array[i] === callback){
-                array.splice(i, 1);
-            };
-        }
+        CuppaStorage.removeCallback({name:this.name, callback:this.onUpdateStorage});
     };
 };
 
-cuppa.removeListenerGroup = function(name){
-    delete cuppa.listeners[name];
-};
-
-cuppa.executeListener = function(name, data){
-    if(!cuppa.listeners[name]) cuppa.listeners[name] = [];
-    let array = cuppa.listeners[name];
-    for(let i = 0 ; i < array.length; i++ ){
-        array[i](data);
-    };
+if(!document.defaultView.GetStorage){
+    customElements.define('get-storage', GetStorage);
+    document.defaultView.GetStorage = GetStorage;
 };
 
 class CuppaStorageInnoDB{
@@ -183,7 +122,7 @@ class CuppaStorageInnoDB{
     constructor(config){
         this.config = {...this.config, ...config};
         this._binAll(this);
-    }
+    };
 
     async connect(){
         let config = this.config;
@@ -198,7 +137,7 @@ class CuppaStorageInnoDB{
                 resolve(this);
             };
         });
-    }
+    };
 
     async onUpdateDB(e){
         this.db = e.target.result;
@@ -206,7 +145,7 @@ class CuppaStorageInnoDB{
         if (!db.objectStoreNames.contains(config.storage)) {
             db.createObjectStore(config.storage, {keyPath: 'name'});
         }
-    }
+    };
 
     async add(name, value, returnValue){
         if(!this.db) await this.connect();
@@ -224,7 +163,7 @@ class CuppaStorageInnoDB{
                 };
         });
         return await this.get(name, returnValue);
-    }
+    };
     
     async get(name, returnValue){
         if(returnValue == undefined) returnValue = true;
@@ -240,7 +179,7 @@ class CuppaStorageInnoDB{
             request.onerror = (err)=>{ resolve(null) };
         });
         return result;
-    }
+    };
 
     async delete(name){
         if(!this.db) await this.connect();
@@ -248,7 +187,7 @@ class CuppaStorageInnoDB{
         let transaction = db.transaction(config.storage, "readwrite");
         let storage = transaction.objectStore(config.storage); 
         storage.delete(name);
-    }
+    };
 
     _binAll(element, isFunction){
         let propertyNames = Object.getOwnPropertyNames(Object.getPrototypeOf(element));
@@ -259,6 +198,6 @@ class CuppaStorageInnoDB{
             };
         };
     };
-}
+};
 
 CuppaStorage.db = new CuppaStorageInnoDB();
