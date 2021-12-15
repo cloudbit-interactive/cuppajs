bb966d1abcfba825763469a653bc143b0aed4456
Tufik Chediak Sanchez <tufik2@gmail.com>
2021-12-15 17:31:26 -0500

    Update components

--- docs/src/cuppa/components/cuppa.alert.js
+++ docs/src/cuppa/components/cuppa.alert.js
@@ -40,20 +40,6 @@ export class CuppaAlert extends CuppaComponent {
 
     render(){
         return html`
-            <style>
-                html, body{ overflow:hidden; }
-                cuppa-alert{ animation-name: cuppa_alert_animation; animation-duration: 0.2s; position:fixed; left:0; right:0; top:0; bottom:0; display:flex; justify-content:center; align-items:center; padding:20px; overflow:auto; }
-                .cuppa_alert_blockade{ background:rgba(0,0,0,0.5); position:fixed; left:0; right:0; top:0; bottom:0; }
-                .cuppa_alert_modal{ position:relative; background: #FFF; width:100%; max-width:500px; padding:30px 40px; overflow: hidden; border-radius: 5px; box-shadow: 0px 3px 10px rgba(0,0,0,0.3); }
-                .cuppa_alert_title{ font-size: 22px; font-weight: 700; margin:0 0 10px; }
-                .cuppa_alert_input{ width:100%; height:32px; width: 100%; margin:10px 0 0; background: #FFF; color: #333; border-radius: 3px; border: 1px solid #CCC; box-shadow:inset 0 1px 1px rgba(0,0,0,.075); padding:0 10px;  font-weight: 500;  }
-                .cuppa_alert_buttons{ margin:10px 0 0; display:flex; justify-content:flex-end; }
-                .cuppa_alert_button{ transition: 0.3s background-color; border: none; cursor:pointer; background: #2F80EC; color:#FFF; height: 42px; padding:0 15px; margin:0 0 0 6px; border-radius: 3px; }
-                .cuppa_alert_button:hover{ background: #1a62c1; }
-                .cuppa_alert_button_cancel{ background:#CCC; color:#333;  }
-                .cuppa_alert_button_cancel:hover{ background:#A0A0A0; }
-                @keyframes cuppa_alert_animation { 0% { opacity: 0; } 100% { opacity: 1; } }
-            </style>
             <div class="cuppa_alert_blockade" @click=${ (this.backdropEnabled === true || this.backdropEnabled === "true") ? ()=>this.onClick(false) : null }></div>
             <div class="cuppa_alert_modal">
                 ${this.title ? html`<div class="cuppa_alert_title">${  html`${this.title}` }</div>` : ''}
@@ -80,6 +66,21 @@ export class CuppaAlert extends CuppaComponent {
                     </div>
                 ` : '' }
             </div>
+            
+            <style>
+                html, body{ overflow:hidden; }
+                cuppa-alert{ animation-name: cuppa_alert_animation; animation-duration: 0.2s; position:fixed; left:0; right:0; top:0; bottom:0; display:flex; justify-content:center; align-items:center; padding:20px; overflow:auto; }
+                .cuppa_alert_blockade{ background:rgba(0,0,0,0.5); position:fixed; left:0; right:0; top:0; bottom:0; }
+                .cuppa_alert_modal{ position:relative; background: #FFF; width:100%; max-width:500px; padding:30px 40px; overflow: hidden; border-radius: 5px; box-shadow: 0px 3px 10px rgba(0,0,0,0.3); }
+                .cuppa_alert_title{ font-size: 22px; font-weight: 700; margin:0 0 10px; }
+                .cuppa_alert_input{ width:100%; height:32px; width: 100%; margin:10px 0 0; background: #FFF; color: #333; border-radius: 3px; border: 1px solid #CCC; box-shadow:inset 0 1px 1px rgba(0,0,0,.075); padding:0 10px;  font-weight: 500;  }
+                .cuppa_alert_buttons{ margin:10px 0 0; display:flex; justify-content:flex-end; }
+                .cuppa_alert_button{ transition: 0.3s background-color; border: none; cursor:pointer; background: #2F80EC; color:#FFF; height: 42px; padding:0 15px; margin:0 0 0 6px; border-radius: 3px; }
+                .cuppa_alert_button:hover{ background: #1a62c1; }
+                .cuppa_alert_button_cancel{ background:#CCC; color:#333;  }
+                .cuppa_alert_button_cancel:hover{ background:#A0A0A0; }
+                @keyframes cuppa_alert_animation { 0% { opacity: 0; } 100% { opacity: 1; } }
+            </style>
             `
     }
 }
