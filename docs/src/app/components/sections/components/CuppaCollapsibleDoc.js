bd46def40fb3365fb568cbc7219e4ff60aea64cd
Tufik Chediak Sanchez <tufik2@gmail.com>
2021-12-22 23:17:06 -0500

    Update component.collapsible

--- docs/src/app/components/sections/components/CuppaCollapsibleDoc.js
+++ docs/src/app/components/sections/components/CuppaCollapsibleDoc.js
@@ -46,7 +46,6 @@ export class CuppaCollapsibleDoc extends CuppaComponent {
                                 <h3 class="title-3">My Title Content</h3>
                                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
                             </cuppa-collapsible-content>
-
                         </cuppa-collapsible>
                     </div>
                     <div style="flex:1">
