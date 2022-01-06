d626761d566c7b333a2b084e66fdf328aef38245
Tufik Chediak Sanchez <tufik2@gmail.com>
2022-01-06 17:22:26 -0500

    Update components

--- docs/src/app/controlers/Globals.js
+++ docs/src/app/controlers/Globals.js
@@ -4,6 +4,7 @@ export class Globals{
     static router = new CuppaRouter({
         root:(document.location.hostname.indexOf('127.0.0.1') != -1) ? 'docs/'
             : (document.location.hostname.indexOf('localhost') != -1) ? 'banks/bank_js/cuppajs/docs/'
+            : (document.location.hostname.indexOf('192.') != -1) ? 'banks/bank_js/cuppajs/docs/'
             : '',
         hash:'#/'
     });
