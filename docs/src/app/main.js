6f907e2b94ab134fbdc7ae67f35dbcadcb3f172d
Tufik Chediak Sanchez <tufik2@gmail.com>
2022-02-11 11:02:15 -0500

    no message

--- docs/src/app/main.js
+++ docs/src/app/main.js
@@ -1,3 +1,7 @@
 import {App} from "./components/App.js";
 
 document.body.append(new App());
+
+if('serviceWorker' in navigator){
+    window.addEventListener('load', ()=>{ navigator.serviceWorker.register('./serviceWorker.js') })
+}
