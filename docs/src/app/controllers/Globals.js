6ab53417d4eb2f0e5a1dfbdaf923d3b867c45060
Tufik Chediak <tufik2@gmail.com>
2025-10-24 17:35:58 -0400

    bug fixes

--- docs/src/app/controllers/Globals.js
+++ docs/src/app/controllers/Globals.js
@@ -1,12 +1,12 @@
 import { CuppaRouter } from "../../cuppa/cuppa.router.min.js";
 
 export class Globals{
-	static version = '0.0.7';
+	static version = '0.0.8';
 	static menuMobile;
 	static router = new CuppaRouter({
-		root: (document.location.hostname.indexOf('localhost') != -1 || document.location.hostname.indexOf('192.') != -1) ?
+		root: (document.location.hostname.indexOf('localhost') !== -1 || document.location.hostname.indexOf('192.') !== -1) ?
 				'banks/bank_js/cuppajs/docs/'
-			: (document.location.hostname.indexOf('127.0.0.1') != -1) ?
+			: (document.location.hostname.indexOf('127.0.0.1') !== -1) ?
 				'docs/'
 			: '',
 		hash:'#/'
