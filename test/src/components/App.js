8b9bd950570161df1af0d367e3a33075cbe1ade8
Tufik Chediak <tufik2@gmail.com>
2026-03-06 17:28:39 -0500

    bug fixes

--- test/src/components/App.js
+++ test/src/components/App.js
@@ -18,12 +18,39 @@ export default class App extends CuppaComponent {
             <div class="flex d-column g-10">
                 <div class="flex d-row j-start a-center g-5 p-x-10 p-y-20 o-hidden bg-warning-light" style="position: relative; height: 30px;">
                     <button class="btnMenu1">Menu1</button>
-                    <cuppa-context-menu target=".btnMenu1">
-	                    <button class="btn1">button1</button>
-                        <button>Button2</button>
-                        <cuppa-context-sub-menu target=".btn1">
-                            <button>button11</button>
+                    <cuppa-context-menu
+	                    target=".btnMenu1"
+	                    pos-x="${CuppaContextMenu.POSITION.LEFT_IN}"
+	                    pos-y="${CuppaContextMenu.POSITION.BOTTOM}"
+                        class="ani-fade-up"
+                    >
+	                    <button class="btn1" >button1</button>
+                        <cuppa-context-sub-menu
+                            target=".btn1"
+                            class="ani-fade-up"
+                        >
+                            <button class="btn11" >button11 </button>
                             <button>Button12</button>
+                            <cuppa-context-sub-menu
+                                target=".btn11"
+                                class="ani-fade-up"
+                            >
+                                <button>button111 </button>
+                                <button>Button112</button>
+                            </cuppa-context-sub-menu>
+                        </cuppa-context-sub-menu>
+	                    
+                        <button class="btn2">Button2</button>
+                        <cuppa-context-sub-menu
+                            target=".btn2"
+                            class="ani-fade-up"
+                        >
+                            <button
+                                @click=${e => {
+                                    e.stopPropagation()
+                                }}>Button21
+                            </button>
+                            <button>Button22</button>
                         </cuppa-context-sub-menu>
                     </cuppa-context-menu>
                 </div>
