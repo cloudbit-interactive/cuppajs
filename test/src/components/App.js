cef97ca6689a6745dcf62e2815c194e9278c4836
Tufik Chediak <tufik2@gmail.com>
2026-03-07 11:48:29 -0500

    bug fixes

--- test/src/components/App.js
+++ test/src/components/App.js
@@ -2,7 +2,7 @@ import { CuppaComponent, html, render as renderHTML } from "../../../libs/cuppa.
 import { cuppa, log } from "../../../libs/cuppa.js";
 import CuppaStorageTest from "./cuppaStorage/CuppaStorageTest.js";
 import "./cuppaComponent/TestComponent.js";
-import { CuppaContextMenu } from "./cuppa.context.menu.js";
+import "./cuppa.dialog.js";
 //import {LitComponent} from "./lit/LitComponent.js";
 
 export default class App extends CuppaComponent {
@@ -16,42 +16,10 @@ export default class App extends CuppaComponent {
     render() {
         return html`
             <div class="flex d-column g-10" style="padding: 800px 0;">
-                <div class="flex d-row j-start a-center g-5 p-x-10 p-y-20 o-hidden bg-warning-light" style="position: relative; height: 30px;">
-                    <button class="btnMenu1">Menu1</button>
-                    <cuppa-context-menu
-	                    target=".btnMenu1"
-                        pos-x="${CuppaContextMenu.POSITION.LEFT_IN}"
-                        pos-y="${CuppaContextMenu.POSITION.BOTTOM}"
-                    >
-                        <button class="btn1">button1</button>
-                        <cuppa-context-sub-menu
-                            target=".btn1"
-                            
-                        >
-                            <button class="button11">button11</button>
-                            <cuppa-context-sub-menu
-	                            target=".button11"
-                            >
-                                <button>button11</button>
-                                <button>Button12</button>
-                                <button>Button13</button>
-                                <button>Button14</button>
-                            </cuppa-context-sub-menu>
-	                        
-                            <button class="btn12">Button12</button>
-                            <cuppa-context-sub-menu
-                                target=".btn12"
-                            >
-                                <button>button121</button>
-                                <button>Button122</button>
-                            </cuppa-context-sub-menu>
-	                        
-                        </cuppa-context-sub-menu>
-	                    
-	                    
-                        <button @click=${e=>{ e.stopPropagation() }}  >Button2</button>
-                    </cuppa-context-menu>
-                </div>
+	            <button class="btn1">Button1</button>
+                <cuppa-dialog force-show="true" target=".btn1"   >
+	                <div style="padding:40px">ddd</div>
+                </cuppa-dialog>
             </div>
 		`
     }
