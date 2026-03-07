d089562797b501c010e3c210540ab29c01daad15
Tufik Chediak <tufik2@gmail.com>
2026-03-07 11:55:31 -0500

    bug fixes

--- test/src/components/App.js
+++ test/src/components/App.js
@@ -2,7 +2,7 @@ import { CuppaComponent, html, render as renderHTML } from "../../../libs/cuppa.
 import { cuppa, log } from "../../../libs/cuppa.js";
 import CuppaStorageTest from "./cuppaStorage/CuppaStorageTest.js";
 import "./cuppaComponent/TestComponent.js";
-import "./cuppa.dialog.js";
+import "./cuppa.select.js";
 //import {LitComponent} from "./lit/LitComponent.js";
 
 export default class App extends CuppaComponent {
@@ -15,11 +15,8 @@ export default class App extends CuppaComponent {
 
     render() {
         return html`
-            <div class="flex d-column g-10" style="padding: 800px 0;">
-	            <button class="btn1">Button1</button>
-                <cuppa-dialog force-show="true" target=".btn1"   >
-	                <div style="padding:40px">ddd</div>
-                </cuppa-dialog>
+            <div class="flex d-column g-10" style="padding: 40px 0;">
+	            <cuppa-select></cuppa-select>
             </div>
 		`
     }
